26c934a21cfe97f772128fea43167cb557bd0103
jjreina <jreinapascual@gmail.com>
2024-12-31 19:46:53 +0100

    Add cleanSelect function to reset select element and update image source

--- client/src/helpers/domHelper.js
+++ client/src/helpers/domHelper.js
@@ -4,3 +4,10 @@ export const createOption = (value, text) => {
   option.text = text;
   return option;
 };
+
+export const cleanSelect = (select, img, text, srcImage) => {
+  select.innerHTML = "";
+  let option = createOption(text, text);
+  select.appendChild(option);
+  img.src = srcImage;
+};
