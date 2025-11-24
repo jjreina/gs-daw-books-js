162136195d2d72a151b03d81b2382cfcff9c4c87
jjreina <jreinapascual@gmail.com>
2025-11-24 10:31:27 +0100

    Refactor promiseHelper.js for improved error handling in parserToGenresClass

--- client/src/helpers/promiseHelper.js
+++ client/src/helpers/promiseHelper.js
@@ -2,7 +2,7 @@ import { Genre } from "../classes/Genre";
 
 export const parserToGenresClass = (literalObjectGenres) => {
   return new Promise((resolve, reject) => {
-    if (literalObjectGenres === null || literalObjectGenres === undefined) {
+    if (literalObjectGenres === null || literalObjectGenres === undefined || literalObjectGenres.length === 0) {
       reject("Error: literalObjectGenres is null or undefined");
     } else {
       const genres = literalObjectGenres.map((genre) => {
